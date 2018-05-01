8ca5772c48cc4cafa36d0a418b3028620dae12f7
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-04-30 21:05:15 -0400

    reworking database

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,39 @@ CREATE DATABASE project3_db;
 
 USE project3_db;
 
-CREATE TABLE auctions(
+
+CREATE TABLE comment_table(
 
   id INT NOT NULL AUTO_INCREMENT,
-
   person_name VARCHAR(100) NOT NULL,
-  comment VARCHAR(100) NOT NULL,
-  location_name VARCHAR(100) NOT NULL,
-  
-  
-
-  -- event_coordinates1 INT,
-  -- event_coordinates2 INT,
-
+  comment_data VARCHAR(100) NOT NULL,
+  location VARCHAR(100) NOT NULL,
   user_LAT FLOAT,
   user_LNG FLOAT,
-  
-
   createdAt TIMESTAMP NOT NULL,
-  
+  PRIMARY KEY (id)
+);
 
+-- (log in)
+CREATE TABLE person_table(
+
+  id INT NOT NULL AUTO_INCREMENT,
+  is_user BOOLEAN DEFAULT true, 
+  person_name VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  -- look at https://developers.facebook.com/docs/facebook-login/web for password
+  email VARCHAR(100) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE event_table(
+
+  id INT NOT NULL AUTO_INCREMENT,
+  event_name VARCHAR(100) NOT NULL,
+  descrption VARCHAR(100) NOT NULL,
+  location_name VARCHAR(100) NOT NULL,
+  event_LAT FLOAT,
+  event_LNG FLOAT,
   PRIMARY KEY (id)
 );
