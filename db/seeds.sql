a3b6d96a4de559af48f8f97b672584e49b179540
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-05-02 19:44:52 -0400

    fixed bugs in seeds and working on find all in apiroutes

--- db/seeds.sql
+++ db/seeds.sql
@@ -52,108 +52,108 @@ VALUES("Festival in the Park", "Charlotte’s oldest (and free) arts festival, t
 --   "308 W Carson Blvd", 35.222098, -80.854827);
 
 -- ==============================================================
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Jane Doe", "There are good inside bathrooms with baby changing table for mothers with young children",
 "bacement of consert hall", 35.20732 , -80.853128);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Jane Doe", "These are the only Recycle bins at the fest",
 "The back of outside seating tent", 35.20710277622387 , -80.85360286878665);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kaspar Weiss", "No line for the German Beer at OMB. Keine Linie fur das deutsche Bier am OMB",
 "OMB tent. OMB-Zelt", 35.15186694441388 , -80.83207975281334);
 -- (Charlotte Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kaspar Weiss", "Good cheep pretzels at the pretzel cart near the water tent.
  gute billige Brezeln auf der Brezel-Wagen in der Nahe von Wasserzelt",
 "OMB tent. OMB-Zelt", 35.151871878779616 , -80.83217865927077);
 -- (Charlotte Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kaspar Weiss", "Discount beer if you buy a meal. Bier Rabatt, wenn Sie eine Mahlzeit kaufen",
 "Gazebo by the lake", 35.1899321681007 , -80.8449367800855);
 -- (Festival in the Park)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "The history lectures are really good.",
 "bacement of consert hall, room 108", 35.20732 , -80.853128);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "Baklava here is cheeper than anywhere else at the festival.",
 "South entrance, room 108", 35.20755971555879 , -80.85337823378165);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "The Porta pottys by the food and beer tent are horrible.",
 "South entrance, room 108", 35.20695155832378 , -80.85351569699367);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "Free Koozies",
 "Sycamore brewery tent", 35.15194150813065 , -80.83231042278862);
 -- (Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "Giving away water bottles",
 "left of main entreance", 35.15215861974028 , -80.83232115162468);
 -- (Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "Only place for recyclables",
 "near water bottle tent", 35.19000231287229 , -80.84451399688953);
 -- (Festival in the Park)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "First Aid tent gives out hydration gum",
 "Medic tent", 35.189719541392044 , -80.84492839818233);
 -- (Festival in the Park)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Kohl Eisenschmidt", "Street neighborhood parking. Two block walk max ",
 "Southside entrance", 35.186317438260424 , -80.8451689299718);
 -- (Festival in the Park)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Philo Mina", "Gyro Great, Greek beer BAD",
 "Food Tent", 35.186317438260424 , -80.8451689299718);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Philo Mina", "Way too crowded to sit and eat ",
 "Dance stage and seating tent", 35.20695594145527 , -80.85338024543842);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Philo Mina", "Get a replacement sipping glass at entrance",
 "entrance", 35.20695594145527 , -80.85338024543842);
 -- (Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Philo Mina", "last place to get Fest Lanyards",
 "Director table", 35.20695594145527 , -80.85338024543842);
 -- (Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Sam Lasky", "Free sunscreen samples at entrance!",
 "Main entrance", 35.20765504790158 , -80.85257692383846);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Sam Lasky", "Three hours is probly too long to stay",
 "Yiasou Greek Festival", 35.20765504790158 , -80.85257692383846);
 -- (Yiasou Greek Festival)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Sam Lasky", "ware closed toe shoes! (ground is all gravel)",
 "Oktoberfest", 35.151698079273366 , -80.83235199702835);
 -- (Oktoberfest)
 
-INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+INSERT INTO comments (person_name, comment_data, location, user_LAT, user_LNG)
 VALUES("Sam Lasky", "Half of homebrew tent is awsome, the other is horrible",
 "Oktoberfest", 35.15166299036901 , -80.83176861656762);
 -- (Oktoberfest)
--- routing/api-routes.js
+++ routing/api-routes.js
@@ -9,27 +9,34 @@ var db = require("../models");
 // Routes
 
 module.exports = function(app) {
-    //Get all markers
-    app.get("/all", function(req, res){
-        db.Comment.findAll({}).then(function(dbComment) {
-            res.json(dbComment);
-        })
+  //Get all markers
+  app.get("/all", function(req, res) {
+    db.Comment.findAll({}).then(function(dbComment) {
+      res.json(dbComment);
     });
+  });
 
-    //Add a marker
-    app.post("/api/new", function(req, res) {
-        console.log("Marker data:");
-        console.log(req.body);
+  //Add a marker
+  app.post("/api/new", function(req, res) {
+    console.log("Marker data:");
+    console.log(req.body);
 
-        db.Comment.create({
-            person_name: req.body.name,
-            comment_data: req.body.comment,
-            location: req.body.location,
-            user_LAT: req.body.lat,
-            user_LNG: req.body.lng,
-            createdAt: req.body.created_at
-        }).then(function(dbComment) {
-            res.json(dbComment)
-        });
-     });
+    db.Comment.create({
+      person_name: req.body.name,
+      comment_data: req.body.comment,
+      location: req.body.location,
+      user_LAT: req.body.lat,
+      user_LNG: req.body.lng,
+      createdAt: req.body.created_at
+    }).then(function(dbComment) {
+      res.json(dbComment);
+    });
+  });
+
+  // GET route for getting all of the comments
+  app.get("/api/comments/", function(req, res) {
+    db.Post.findAll({}).then(function(dbPost) {
+      res.json(dbPost);
+    });
+  });
 };
