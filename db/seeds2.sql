f7810784e29fc81220ace88cb16b0bf06711116e
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-05-02 13:32:16 -0400

    created first seed data

---
+++ db/seeds2.sql
@@ -0,0 +1,36 @@
+
+
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Jane Doe", "There are good inside bathrooms with baby changing table for mothers with young children",
+"bacement of consert hall", 35.20732 , -80.853128);
+-- (Yiasou Greek Festival)
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Jane Doe", "These are the only Recycle bins at the fest",
+"The back of outside seating tent", 35.20710277622387 , -80.85360286878665);
+-- (Yiasou Greek Festival)
+
+
+
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Kohl Eisenschmidt", "The history lectures are really good.",
+"bacement of consert hall, room 108", 35.20732 , -80.853128);
+-- (Yiasou Greek Festival)
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Kohl Eisenschmidt", "Baklava here is cheeper than anywhere else at the festival.",
+"South entrance, room 108", 35.20755971555879 , -80.85337823378165);
+-- (Yiasou Greek Festival)
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Kohl Eisenschmidt", "The Porta pottys by the food and beer tent are horrible.",
+"South entrance, room 108", 35.20695155832378 , -80.85351569699367);
+-- (Yiasou Greek Festival)
+
+
+
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Philo Mina", "Gyro Great, Greek beer BAD",
+"Food Tent", 35.186317438260424 , -80.8451689299718);
+-- (Yiasou Greek Festival)
+INSERT INTO comment_table (person_name, comment_data, location, user_LAT, user_LNG, createdAt)
+VALUES("Philo Mina", "Way too crowded to sit and eat ",
+"Dance stage and seating tent", 35.20695594145527 , -80.85338024543842);
+-- (Yiasou Greek Festival)
