a73b06d423a169910fd78e9465977322774cc761
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-05-03 12:09:46 -0400

    some minor updates

--- db/schema.sql
+++ db/schema.sql
@@ -4,39 +4,39 @@ CREATE DATABASE project3_db;
 USE project3_db;
 
 
-CREATE TABLE comment_table(
+-- CREATE TABLE comment_table(
 
-  id INT NOT NULL AUTO_INCREMENT,
-  person_name VARCHAR(100) NOT NULL,
-  comment_data VARCHAR(100) NOT NULL,
-  location VARCHAR(100) NOT NULL,
-  user_LAT FLOAT,
-  user_LNG FLOAT,
-  createdAt TIMESTAMP NOT NULL,
-  PRIMARY KEY (id)
-);
+--   id INT NOT NULL AUTO_INCREMENT,
+--   person_name VARCHAR(100) NOT NULL,
+--   comment_data VARCHAR(100) NOT NULL,
+--   location VARCHAR(100) NOT NULL,
+--   user_LAT FLOAT,
+--   user_LNG FLOAT,
+--   createdAt TIMESTAMP NOT NULL,
+--   PRIMARY KEY (id)
+-- );
 
--- (log in)
-CREATE TABLE person_table(
--- server knows both ids
-  id INT NOT NULL AUTO_INCREMENT,
-  -- user 
-  firebase_id 
-  person_name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  password VARCHAR(100) NOT NULL,
-  is_user BOOLEAN DEFAULT true, 
-  -- look at https://developers.facebook.com/docs/facebook-login/web for password
-  PRIMARY KEY (id)
-);
+-- -- (log in)
+-- CREATE TABLE person_table(
+-- -- server knows both ids
+--   id INT NOT NULL AUTO_INCREMENT,
+--   -- user 
+--   firebase_id 
+--   person_name VARCHAR(100) NOT NULL,
+--   email VARCHAR(100) NOT NULL,
+--   password VARCHAR(100) NOT NULL,
+--   is_user BOOLEAN DEFAULT true, 
+--   -- look at https://developers.facebook.com/docs/facebook-login/web for password
+--   PRIMARY KEY (id)
+-- );
 
 
-CREATE TABLE event_table(
+-- CREATE TABLE event_table(
 
-  id INT NOT NULL AUTO_INCREMENT,
+  event_id INT NOT NULL AUTO_INCREMENT,
   event_name VARCHAR(100) NOT NULL,
-  descrption VARCHAR(100) NOT NULL,
-  location_name VARCHAR(100) NOT NULL,
+  -- descrption VARCHAR(100) NOT NULL,
+  -- location_name VARCHAR(100) NOT NULL,
   event_LAT FLOAT,
   event_LNG FLOAT,
   PRIMARY KEY (id)
