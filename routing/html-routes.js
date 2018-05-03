3905c340723ad277215caba17e8002126baedb82
pmina <philomn9@gmail.com>
2018-05-03 12:07:15 -0400

    add event drop down

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -4,10 +4,6 @@ var path = require("path");
 module.exports = function(app) {
   //homepage
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../testMap3.html"));
-  });
-  //homepage
-  app.get("/map", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/mapTest.html"));
   });
 };
