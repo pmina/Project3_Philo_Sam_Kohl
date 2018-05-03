e579236e4119eef400601a713c7c5aebd3a6ff6d
Kohl Eisenschmidt <kohleisenschmidt@gmail.com>
2018-05-03 19:27:33 -0400

    test

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -2,12 +2,12 @@ var path = require("path");
 // var server = require("/server.js");
 
 module.exports = function(app) {
-  //homepage
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/login.html"));
-  // });
-  //homepage
+  // homepage
   app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  // homepage
+  app.get("/map", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/map.html"));
   });
 };
